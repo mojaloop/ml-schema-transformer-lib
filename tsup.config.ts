065ea5536681d3eb788c3425ce9af6fbe4d3e210
Steven Oderayi <oderayi@gmail.com>
2024-09-25 11:49:11 +0100

    ci: update dependencies and other toolings

---
+++ tsup.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  format: ['cjs', 'esm'],
+  entry: ['./src/**/*.ts'],
+  outDir: 'dist',
+  dts: true,
+  sourcemap: true,
+  shims: true,
+  skipNodeModulesBundle: true,
+  clean: true,
+});
