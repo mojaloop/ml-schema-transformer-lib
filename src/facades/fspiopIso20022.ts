7d9a49dec962aa10dac952944d2b8240e261c2cf
Steven Oderayi <oderayi@gmail.com>
2024-09-26 20:37:05 +0100

    fix: fix mapping path

--- src/facades/fspiopIso20022.ts
+++ src/facades/fspiopIso20022.ts
@@ -14,8 +14,8 @@ export const Fspiop20022TransformFacade = {
     log = logger;
   },
   parties: {
-    put: async (payload: unknown, mapping: OverrideMapping = undefined) => transformFn(payload, mapping || discovery.put, log),
-    putError: async (payload: unknown, mapping: OverrideMapping = undefined) => transformFn(payload, mapping || discovery.putError, log)
+    put: async (payload: unknown, mapping: OverrideMapping = undefined) => transformFn(payload, mapping || discovery.parties.put, log),
+    putError: async (payload: unknown, mapping: OverrideMapping = undefined) => transformFn(payload, mapping || discovery.parties.putError, log)
   },
   quotes: {
     post: async (payload: unknown, mapping: OverrideMapping = undefined) => transformFn(payload, mapping || quotes.post, log),
