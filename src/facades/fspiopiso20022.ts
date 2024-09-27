62a3c7351f2a58521c17838be87336f10da419e7
Steven Oderayi <oderayi@gmail.com>
2024-09-27 17:15:40 +0100

    chore: lint

--- src/facades/fspiopiso20022.ts
+++ src/facades/fspiopiso20022.ts
@@ -22,44 +22,44 @@
  --------------
  ******/
 
- import { ContextLogger } from '@mojaloop/central-services-logger/src/contextLogger';
- import { discovery, quotes, fxQuotes, transfers, fxTransfers } from '../mappings/fspiopiso20022';
- import { logger as defaultLogger } from '../lib/logger';
- import { transformFn } from '../lib/transformer';
- import { OverrideMapping } from '../types';
+import { ContextLogger } from '@mojaloop/central-services-logger/src/contextLogger';
+import { discovery, quotes, fxQuotes, transfers, fxTransfers } from '../mappings/fspiopiso20022';
+import { logger as defaultLogger } from '../lib/logger';
+import { transformFn } from '../lib/transformer';
+import { OverrideMapping } from '../types';
  
- let log = defaultLogger;
+let log = defaultLogger;
  
- /**
+/**
   * Facades for transforming FSPIOP ISO 20022 payloads to FSPIOP payloads
   */
- export const FspiopIso20022TransformFacade = {
-   configure: ({ logger }: { logger: ContextLogger }) => {
-     log = logger;
-   },
-   parties: {
-     put: async (payload: unknown, mapping: OverrideMapping = undefined) => transformFn(payload, mapping || discovery.parties.put, log),
-     putError: async (payload: unknown, mapping: OverrideMapping = undefined) => transformFn(payload, mapping || discovery.parties.putError, log)
-   },
-   quotes: {
-     post: async (payload: unknown, mapping: OverrideMapping = undefined) => transformFn(payload, mapping || quotes.post, log),
-     put: async (payload: unknown, mapping: OverrideMapping = undefined) => transformFn(payload, mapping || quotes.put, log),
-     putError: async (payload: unknown, mapping: OverrideMapping = undefined) => transformFn(payload, mapping || quotes.putError, log)
-   },
-   fxQuotes: {
-     post: async (payload: unknown, mapping: OverrideMapping = undefined) => transformFn(payload, mapping || fxQuotes.post, log),
-     put: async (payload: unknown, mapping: OverrideMapping = undefined) => transformFn(payload, mapping || fxQuotes.put, log),
-     putError: async (payload: unknown, mapping: OverrideMapping = undefined) => transformFn(payload, mapping || fxQuotes.putError, log)
-   },
-   transfers: {
-     post: async (payload: unknown, mapping: OverrideMapping = undefined) => transformFn(payload, mapping || transfers.post, log),
-     put: async (payload: unknown, mapping: OverrideMapping = undefined) => transformFn(payload, mapping || transfers.put, log),
-     putError: async (payload: unknown, mapping: OverrideMapping = undefined) => transformFn(payload, mapping || transfers.putError, log)
-   },
-   fxTransfers: {
-     post: async (payload: unknown, mapping: OverrideMapping = undefined) => transformFn(payload, mapping || fxTransfers.post, log),
-     put: async (payload: unknown, mapping: OverrideMapping = undefined) => transformFn(payload, mapping || fxTransfers.put, log),
-     putError: async (payload: unknown, mapping: OverrideMapping = undefined) => transformFn(payload, mapping || fxTransfers.putError, log)
-   }
- };
+export const FspiopIso20022TransformFacade = {
+  configure: ({ logger }: { logger: ContextLogger }) => {
+    log = logger;
+  },
+  parties: {
+    put: async (payload: unknown, mapping: OverrideMapping = undefined) => transformFn(payload, mapping || discovery.parties.put, log),
+    putError: async (payload: unknown, mapping: OverrideMapping = undefined) => transformFn(payload, mapping || discovery.parties.putError, log)
+  },
+  quotes: {
+    post: async (payload: unknown, mapping: OverrideMapping = undefined) => transformFn(payload, mapping || quotes.post, log),
+    put: async (payload: unknown, mapping: OverrideMapping = undefined) => transformFn(payload, mapping || quotes.put, log),
+    putError: async (payload: unknown, mapping: OverrideMapping = undefined) => transformFn(payload, mapping || quotes.putError, log)
+  },
+  fxQuotes: {
+    post: async (payload: unknown, mapping: OverrideMapping = undefined) => transformFn(payload, mapping || fxQuotes.post, log),
+    put: async (payload: unknown, mapping: OverrideMapping = undefined) => transformFn(payload, mapping || fxQuotes.put, log),
+    putError: async (payload: unknown, mapping: OverrideMapping = undefined) => transformFn(payload, mapping || fxQuotes.putError, log)
+  },
+  transfers: {
+    post: async (payload: unknown, mapping: OverrideMapping = undefined) => transformFn(payload, mapping || transfers.post, log),
+    put: async (payload: unknown, mapping: OverrideMapping = undefined) => transformFn(payload, mapping || transfers.put, log),
+    putError: async (payload: unknown, mapping: OverrideMapping = undefined) => transformFn(payload, mapping || transfers.putError, log)
+  },
+  fxTransfers: {
+    post: async (payload: unknown, mapping: OverrideMapping = undefined) => transformFn(payload, mapping || fxTransfers.post, log),
+    put: async (payload: unknown, mapping: OverrideMapping = undefined) => transformFn(payload, mapping || fxTransfers.put, log),
+    putError: async (payload: unknown, mapping: OverrideMapping = undefined) => transformFn(payload, mapping || fxTransfers.putError, log)
+  }
+};
  
