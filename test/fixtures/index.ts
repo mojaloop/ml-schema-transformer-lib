e613c440989dbe825548f15e7aac30d72e2b8ee5
Steven Oderayi <oderayi@gmail.com>
2024-10-14 12:18:35 +0100

    chore: linting

--- test/fixtures/index.ts
+++ test/fixtures/index.ts
@@ -1725,7 +1725,7 @@ export const expectedFspiopIso20022Targets = (target: GenericObject) => ({
           MsgId: getProp(target, 'body.GrpHdr.MsgId'),
           CreDtTm: getProp(target, 'body.GrpHdr.CreDtTm'),
           NbOfTxs: '1',
-          PmtInstrXpryDtTm: "2016-05-24T08:38:08.699-04:00",
+          PmtInstrXpryDtTm: '2016-05-24T08:38:08.699-04:00',
           SttlmInf: {
             SttlmMtd: 'CLRG'
           }
@@ -1869,7 +1869,7 @@ export const expectedFspiopIso20022Targets = (target: GenericObject) => ({
           ChrgBr: 'DEBT'
         },
         GrpHdr: {
-          "PmtInstrXpryDtTm": "2016-05-24T08:38:08.699-04:00"
+          PmtInstrXpryDtTm: '2016-05-24T08:38:08.699-04:00'
         }
       }
     },
