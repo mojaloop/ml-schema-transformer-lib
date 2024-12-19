061d33c4aa9251c9c071470c767633d728f95e1c
Steven Oderayi <oderayi@gmail.com>
2024-12-19 12:25:21 +0100

    chore: linting

--- test/unit/random.test.ts
+++ test/unit/random.test.ts
@@ -1,7 +1,5 @@
 import { createTransformer } from 'src';
 import { FspiopTransformFacade, FspiopIso20022TransformFacade } from '../../src/facades';
-import { FSPIO20022PMappings } from '../../src/mappings';
-
 
 describe('Random', () => {
   it.skip('should transform with alt and fixed value', async () => {
@@ -98,72 +96,70 @@ describe('Random', () => {
   });
 
   it.skip('should transform with alt and fixed value', async () => {
-    const mappingStr = FSPIO20022PMappings.discovery.parties.put;
-    const mapping = JSON.parse(mappingStr);
     const transformer = FspiopIso20022TransformFacade.parties.put;
     const request = {
-      "url": "http://172.17.0.1:3001/parties/MSISDN/16135551002",
-      "uniqueId": "1734604735552pqveh",
-      "headers": {
-        "Content-Type": "application/vnd.interoperability.iso20022.parties+json;version=2.0",
-        "FSPIOP-Source": "testingtoolkitdfsp",
-        "Date": "Thu, 19 Dec 2024 10:38:55 GMT",
-        "FSPIOP-Destination": "dfsp1",
-        "traceparent": "00-ccdd39191b8fa41a3a394824f4a9b1-0123456789abcdef0-00"
+      url: 'http://172.17.0.1:3001/parties/MSISDN/16135551002',
+      uniqueId: '1734604735552pqveh',
+      headers: {
+        'Content-Type': 'application/vnd.interoperability.iso20022.parties+json;version=2.0',
+        'FSPIOP-Source': 'testingtoolkitdfsp',
+        Date: 'Thu, 19 Dec 2024 10:38:55 GMT',
+        'FSPIOP-Destination': 'dfsp1',
+        traceparent: '00-ccdd39191b8fa41a3a394824f4a9b1-0123456789abcdef0-00'
       },
-      "body": {
-        "Assgnmt": {
-          "MsgId": "01JFF7ZH1Z5J2A7RP7B2MC59HE",
-          "CreDtTm": "2024-12-19T10:38:55.546Z",
-          "Assgnr": {
-            "Agt": {
-              "FinInstnId": {
-                "Othr": {
-                  "Id": "dfsp1"
+      body: {
+        Assgnmt: {
+          MsgId: '01JFF7ZH1Z5J2A7RP7B2MC59HE',
+          CreDtTm: '2024-12-19T10:38:55.546Z',
+          Assgnr: {
+            Agt: {
+              FinInstnId: {
+                Othr: {
+                  Id: 'dfsp1'
                 }
               }
             }
           },
-          "Assgne": {
-            "Agt": {
-              "FinInstnId": {
-                "Othr": {
-                  "Id": "testingtoolkitdfsp"
+          Assgne: {
+            Agt: {
+              FinInstnId: {
+                Othr: {
+                  Id: 'testingtoolkitdfsp'
                 }
               }
             }
           }
         },
-        "Rpt": {
-          "OrgnlId": "MSISDN/16135551002",
-          "Vrfctn": true,
-          "UpdtdPtyAndAcctId": {
-            "Agt": {
-              "FinInstnId": {
-                "LEI": "529900T8BM49AURSDO55",
-                "Othr": {
-                  "Id": "ttkpayeefsp"
+        Rpt: {
+          OrgnlId: 'MSISDN/16135551002',
+          Vrfctn: true,
+          UpdtdPtyAndAcctId: {
+            Agt: {
+              FinInstnId: {
+                LEI: '529900T8BM49AURSDO55',
+                Othr: {
+                  Id: 'ttkpayeefsp'
                 }
               }
             },
-            "Acct": {
-              "Ccy": "XTS"
+            Acct: {
+              Ccy: 'XTS'
             },
-            "Pty": {
-              "PstlAdr": {
-                "StrtNm": "1",
-                "PstCd": "XXXXXX",
-                "Ctry": "ZM",
-                "AdrLine": "Main road, Big Town"
+            Pty: {
+              PstlAdr: {
+                StrtNm: '1',
+                PstCd: 'XXXXXX',
+                Ctry: 'ZM',
+                AdrLine: 'Main road, Big Town'
               },
-              "CtryOfRes": "ZM",
-              "Nm": "Test Party",
-              "Id": {
-                "PrvtId": {
-                  "Othr": {
-                    "Id": "16135551002",
-                    "SchmeNm": {
-                      "Prtry": "MSISDN"
+              CtryOfRes: 'ZM',
+              Nm: 'Test Party',
+              Id: {
+                PrvtId: {
+                  Othr: {
+                    Id: '16135551002',
+                    SchmeNm: {
+                      Prtry: 'MSISDN'
                     }
                   }
                 }
@@ -189,7 +185,7 @@ describe('Random', () => {
         'fspiop-destination': 'destinationfsp'
       }
     };
-    const target2 = await transformer.transform(source2, {});
+    const target2 = await transformer(source2, {});
     /* eslint-disable-next-line */
     console.log(target2);
   });
