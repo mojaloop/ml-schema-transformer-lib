3c644aad88838e46d65fa70ed2bd423c720e71fc
Kalin Krustev <kalin.krustev@gmail.com>
2026-01-04 15:26:06 +0200

    fix: handle subtype mapping (#63)

--- test/unit/lib/validation/validator.test.ts
+++ test/unit/lib/validation/validator.test.ts
@@ -12,13 +12,13 @@ describe('Validator', () => {
   beforeAll(async () => {
     validator = new Validator(specPath);
     await validator.initialize()
-  });
+  }, 20_000);
 
   it('should initialize the validator', async () => {
     await validator.initialize();
     expect(validator.apiSpec).toBeDefined();
     expect(validator.apiValidator).toBeDefined();
-  }, 10_000);
+  }, 20_000);
 
   describe('validateRequest', () => {
     it('should throw an error if validation fails', () => {
@@ -47,7 +47,7 @@ describe('Validator', () => {
       const request = {
         path: '/transfers',
         method: HTTP_METHOD.POST,
-        headers: { 
+        headers: {
           'accept': 'application/vnd.interoperability.transfers+json;version=2.0',
           'content-type': 'application/vnd.interoperability.transfers+json;version=2.0',
           'date': new Date().toISOString(),
@@ -60,7 +60,7 @@ describe('Validator', () => {
       expect(result).toBe(true);
     });
   })
-  
+
   describe('validateBody', () => {
     it('should throw an error if validation fails', () => {
       const partialRequest = {
@@ -111,7 +111,7 @@ describe('Validator', () => {
     });
     it('should validate the headers', () => {
       const partialRequest = {
-        headers: { 
+        headers: {
           'accept': 'application/vnd.interoperability.transfers+json;version=2.0',
           'content-type': 'application/vnd.interoperability.transfers+json;version=2.0',
           'date': new Date().toISOString(),
